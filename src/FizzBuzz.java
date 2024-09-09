1e1df431b4a820552dee994ba5089db56851c4ae
phytorchumsworth <AliKarimLalani@github.com>
2024-09-09 18:36:15 -0400

    blah blah

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,7 +6,7 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i=1;
         while (i<100) {
-
+            //Hi
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
